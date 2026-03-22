e2b28e9a6e2c300ab4dd64d966e57a96392d675d
Roshan <roshankrsu@gmail.com>
2026-03-22 17:42:14 +0530

    data model

---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "main-stackflow"
+export const questionCollection = "quesions"
+export const answerCollection = "answers"
+export const commentCollection = "comments"
+export const voteCollection = "votes"
+export const questionAttachmentBucket = "question-attachment"
