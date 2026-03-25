a769ee97501b3f958835f85e3e42ef4c3acc5956
Roshan <roshankrsu@gmail.com>
2026-03-25 10:49:05 +0530

    added appwrite domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sgp.cloud.appwrite.io",
+        pathname: "/v1/storage/**",
       },
     ],
   },
